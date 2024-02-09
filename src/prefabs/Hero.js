ea07801aa6044a99fc04a3a7cc0283fdc9c673b8
Acer Cristea <acristea@ucsc.edu>
2024-02-09 15:33:42 -0800

    Adding delay to shake

--- src/prefabs/Hero.js
+++ src/prefabs/Hero.js
@@ -205,6 +205,7 @@ class CircularState extends State {
         //hero.setTint(0x0000FF)
         hero.setVelocity(0)
         hero.anims.play("spin-attack").once("animationcomplete", ()=> {
+            scene.cameras.main.shake(150,0.005)
             this.stateMachine.transition("idle")
         })
         scene.cameras.main.shake(150,0.005)
